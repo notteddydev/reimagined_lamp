6fc716edb17f464f9c7c56fa74a7d2c914412312
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-29 13:06:21 +0100

    Basic views for contact-new and contact-list, templates too.

--- address_book/urls.py
+++ address_book/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.ContactListView.as_view(), name="contact-list"),
+    path("contacts/new", views.ContactCreateView.as_view(), name="contact-create"),
 ]
--- address_book/views.py
+++ address_book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+from .models import Contact
+
+class ContactListView(ListView):
+    model = Contact
+
+
+class ContactCreateView(CreateView):
+    model = Contact
+    fields = '__all__'
