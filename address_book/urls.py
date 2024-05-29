2632f2c4cd1483f48212927bd7b558ba023e734c
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-29 18:08:18 +0100

    Register ContactDetailView url.

--- address_book/urls.py
+++ address_book/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.ContactListView.as_view(), name="contact-list"),
     path("contacts/new", views.ContactCreateView.as_view(), name="contact-create"),
+    path("contacts/<int:pk>", views.ContactDetailView.as_view(), name="contact-detail"),
 ]
