6d7eab6f5a4264df7c6cf7eccb0c8664847e5e84
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-29 18:09:01 +0100

    Use OwnedByUserMixin and UserPassesTestMixin to prevent users viewing other users contacts.

--- address_book/views.py
+++ address_book/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import CreateView, DetailView, ListView
 
 from .models import Contact
+from app.mixins import OwnedByUserMixin
 
-class ContactListView(ListView):
+class ContactListView(LoginRequiredMixin, OwnedByUserMixin, ListView):
     model = Contact
-
-
-class ContactCreateView(CreateView):
+    
+class ContactCreateView(LoginRequiredMixin, CreateView):
     model = Contact
     fields = '__all__'
+
+# TODO
+# Can use OwnedByUserMixin here too. Leave as is for timebeing until there is an
+# appropriate place to leave an example of UserPassesTestMixin too.
+class ContactDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Contact
+
+    def test_func(self) -> bool | None:
+        return Contact.objects.filter(id=self.kwargs['pk'], user=self.request.user).exists()
