58bf463a57a3cb864c3e3634d85b7ec53b5c374b
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-05-29 08:48:49 +0100

    Basic homepage setup.

--- app/urls.py
+++ app/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('address-book/', include('address_book.urls')),
     path('', include('django.contrib.auth.urls')),
+    path('', views.HomeView.as_view()),
 ]
