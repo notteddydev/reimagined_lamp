ee767da8eccb7c776efc9ca904eb7550f9b07aa2
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-06-14 20:01:45 +0100

    Remove addresses from contact form. Will do that elsewhere. Update met_through_contact field to tags field.

--- address_book/forms.py
+++ address_book/forms.py
@@ -1,13 +1,12 @@
 from django.forms import ModelForm
 
-from .models import Address, Contact
+from .models import Contact, Tag
 
 class ContactForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(self, *args, **kwargs)
-        self.fields['addresses'].queryset = Address.objects.filter(user=self.initial['user'])
         self.fields['family_members'].queryset = Contact.objects.filter(user=self.initial['user'])
-        self.fields['met_through_contact'].queryset = Contact.objects.filter(user=self.initial['user'])
+        self.fields['tags'].queryset = Tag.objects.filter(user=self.initial['user'])
 
     # TODO
     # Find out why this is needed and if it should be different.
@@ -16,5 +15,5 @@ class ContactForm(ModelForm):
         # super().get(self, request)
 
     class Meta:
-        exclude = ['user']
+        exclude = ['addresses', 'user']
         model = Contact
