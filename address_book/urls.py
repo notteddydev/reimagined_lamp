440bf8c7f5906db5720b20b624db860c34d76b32
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-06-14 17:49:06 +0100

    Change contact-list url to follow convention.

--- address_book/urls.py
+++ address_book/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ContactListView.as_view(), name="contact-list"),
+    path("contacts/", views.ContactListView.as_view(), name="contact-list"),
     path("contacts/new", views.ContactCreateView.as_view(), name="contact-create"),
     path("contacts/<int:pk>", views.ContactDetailView.as_view(), name="contact-detail"),
 ]
