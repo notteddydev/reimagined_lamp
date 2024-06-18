74f40bcba28671d144b3d264fbe735866959b18d
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-06-18 15:14:51 +0100

    Accept user as a kwarg rather than as part of 'initial' dictionary.

--- address_book/forms.py
+++ address_book/forms.py
@@ -3,10 +3,11 @@ from django import forms
 from .models import Contact, Tag
 
 class ContactForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, user, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
-        self.fields['family_members'].queryset = Contact.objects.filter(user=self.initial['user'])
-        self.fields['tags'].queryset = Tag.objects.filter(user=self.initial['user'])
+        self.fields['family_members'].queryset = Contact.objects.filter(user=user)
+        self.fields['tags'].queryset = Tag.objects.filter(user=user)
+        self.instance.user_id = user.id
 
     class Meta:
         model = Contact
