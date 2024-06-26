ca9d33bdb6af0eb1662415df31fb6cd823399f0f
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-06-26 10:38:22 +0100

    Create PhoneNumberForm and PhoneNumberFormset to add PhoneNumbers to a Contact.

--- address_book/forms.py
+++ address_book/forms.py
@@ -1,14 +1,25 @@
 from django import forms
 
-from .models import Contact, Tag
+from phonenumber_field.formfields import SplitPhoneNumberField
+
+from .models import Contact, PhoneNumber, Tag
+
+class PhoneNumberForm(forms.ModelForm):
+    number = SplitPhoneNumberField()
+
+    class Meta:
+        model = PhoneNumber
+        exclude = ['contact']
 
 class ContactForm(forms.ModelForm):
     def __init__(self, user, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
-        self.fields['family_members'].queryset = Contact.objects.filter(user=user)
-        self.fields['tags'].queryset = Tag.objects.filter(user=user)
         self.instance.user_id = user.id
+        self.fields['tags'].queryset = Tag.objects.filter(user=user.id)
+        self.fields['family_members'].queryset = Contact.objects.filter(user=user.id)
 
     class Meta:
         model = Contact
         exclude = ['addresses', 'user']
+
+PhoneNumberFormSet = forms.inlineformset_factory(Contact, PhoneNumber, PhoneNumberForm, extra=3, can_delete=True)
