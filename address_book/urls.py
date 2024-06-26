23a40f5f45bf7c3b57eb8c2b5fb2866de80d46ec
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-06-26 12:15:24 +0100

    Alphabetise urls and add contact-update url.

--- address_book/urls.py
+++ address_book/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("contacts/", views.ContactListView.as_view(), name="contact-list"),
     path("contacts/new", views.ContactCreateView.as_view(), name="contact-create"),
     path("contacts/<int:pk>", views.ContactDetailView.as_view(), name="contact-detail"),
+    path("contacts/", views.ContactListView.as_view(), name="contact-list"),
+    path("contacts/<int:pk>/edit", views.ContactUpdateView.as_view(), name="contact-update")
 ]
