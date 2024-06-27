6d582258dfd3a5fab6a9b8fe19ba480c3b53e1aa
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-06-27 16:51:38 +0100

    Urls for new Tag views.

--- address_book/urls.py
+++ address_book/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("contacts/new", views.ContactCreateView.as_view(), name="contact-create"),
     path("contacts/<int:pk>", views.ContactDetailView.as_view(), name="contact-detail"),
     path("contacts/", views.ContactListView.as_view(), name="contact-list"),
-    path("contacts/<int:pk>/edit", views.ContactUpdateView.as_view(), name="contact-update")
+    path("contacts/<int:pk>/edit", views.ContactUpdateView.as_view(), name="contact-update"),
+    path("tags/", views.TagListView.as_view(), name="tag-list"),
+    path("tags/new", views.TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>", views.TagDetailView.as_view(), name="tag-detail"),
 ]
