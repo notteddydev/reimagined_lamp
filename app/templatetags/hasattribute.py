d60629c316909bc8390cccbb549cf1395062bca3
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-07-05 16:43:21 +0100

    Update hasattribute templatetag so that it reflects its name better.

--- app/templatetags/hasattribute.py
+++ app/templatetags/hasattribute.py
@@ -5,6 +5,6 @@ register = template.Library()
 def hasattribute(value, arg):
     """Determines whether an object has an attribute dynamically from a string name"""
 
-    return hasattr(value, str(arg)) and bool(getattr(value, str(arg)))
+    return hasattr(value, str(arg))
 
 register.filter('hasattribute', hasattribute)
