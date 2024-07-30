f247e1ba4e3e8fa8d1733ab9a7b0747ec4f23ed5
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-07-30 09:49:46 +0100

    Add download url.

--- address_book/urls.py
+++ address_book/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("contacts/new", views.ContactCreateView.as_view(), name="contact-create"),
     path("contacts/<int:pk>", views.ContactDetailView.as_view(), name="contact-detail"),
     path("contacts/", views.contact_list_view, name="contact-list"),
+    path("contacts/download", views.contact_list_download_view, name="contact-list-download"),
     path("contacts/<int:pk>/edit", views.ContactUpdateView.as_view(), name="contact-update"),
     path("contacts/<int:pk>/qrcode", views.contact_qrcode_view, name="contact-qrcode"),
     path("contacts/<int:pk>/download", views.contact_download_view, name="contact-download"),
