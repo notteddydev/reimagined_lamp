f61b6076ea2dd4fb79ab237506d5e329c0c5f429
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-08-22 17:20:14 -0400

    Make logic more readable in AddressCreateView.

--- address_book/views.py
+++ address_book/views.py
@@ -118,11 +118,12 @@ class AddressCreateView(LoginRequiredMixin, View):
         if form.is_valid() and phonenumber_formset.is_valid():
             address = form.save()
 
-            phonenumber_formset_is_empty = all(
-                not form.cleaned_data for form in phonenumber_formset if form.is_valid()
+            has_valid_data = any(
+                form.is_valid() and form.cleaned_data
+                for form in phonenumber_formset
             )
 
-            if not phonenumber_formset_is_empty:
+            if has_valid_data:
                 phonenumber_formset.instance = address
                 phonenumber_formset.save()
 
