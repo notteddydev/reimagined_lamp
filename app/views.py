939c25c92f9249100db14ddcd43c0858552c81ea
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-08-23 08:35:46 -0400

    Remove redundant imports.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic import TemplateView
