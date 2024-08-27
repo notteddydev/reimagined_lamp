94b3f0d3c904ef951eb652c9662d0f8bd24652f7
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-08-27 15:26:29 -0400

    Create UserFactory.

---
+++ address_book/factories/user_factories.py
@@ -0,0 +1,10 @@
+import factory
+from django.contrib.auth.models import User
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username: str = factory.Faker("user_name")
+    email: str = factory.Faker("email")
+    password: str = factory.PostGenerationMethodCall("set_password", "password123")
