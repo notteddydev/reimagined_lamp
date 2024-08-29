d0d0723a8089aa4fc0b86b92a4cc61f209f5093c
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-08-29 16:09:10 -0400

    Add utils file, and test_utils file. Add new util method: get_years_from_year. Add tests for it.

---
+++ address_book/tests/test_utils.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from django.test import TestCase
+
+from address_book.utils import get_years_from_year
+
+class TestGetYearsFromYear(TestCase):
+    def setUp(self) -> None:
+        self.current_year = datetime.now().year
+
+    def test_no_args(self) -> None:
+        """
+        Test that the method returns the expected list of years when no args are passed to the method.
+        """
+        years = get_years_from_year()
+        
+        year = 1920
+        while year <= self.current_year:
+            self.assertIn(year, years)
+            year += 1
+
+        self.assertEqual(years[0], self.current_year)
+        self.assertEqual(years[-1], 1920)
+
+    def test_custom_year(self) -> None:
+        """
+        Test that the method returns the expected list of years when a 'year' arg is passed to the method.
+        """
+        years = get_years_from_year(year=2000)
+        
+        year = 2000
+        while year <= self.current_year:
+            self.assertIn(year, years)
+            year += 1
+
+        self.assertEqual(years[0], self.current_year)
+        self.assertEqual(years[-1], 2000)
+
+    def test_asc(self) -> None:
+        """
+        Test that the method returns the expected list of years when the 'desc' arg is passed
+        to the method as False.
+        """
+        years = get_years_from_year(desc=False)
+        
+        year = 1920
+        while year <= self.current_year:
+            self.assertIn(year, years)
+            year += 1
+
+        self.assertEqual(years[0], 1920)
+        self.assertEqual(years[-1], self.current_year)
+
+    def test_future_year_raises_exception(self) -> None:
+        """
+        Test that the an exception is raised if a 'year' arg is passed in with a value greater
+        than that of the current year.
+        """
+        with self.assertRaises(ValueError) as cm:
+            get_years_from_year(year=self.current_year + 1)
+
+        self.assertEqual(str(cm.exception), "The year provided must be earlier than the current year.")
---
+++ address_book/utils.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from typing import List, Optional
+
+def get_years_from_year(year: Optional[int] = 1920, desc: Optional[bool] = True) -> List[int]:
+    current_year = datetime.now().year
+
+    if year > current_year:
+        raise ValueError("The year provided must be earlier than the current year.")
+
+    years = [year for year in range(year, current_year + 1)]
+
+    return years[::-1] if desc else years
