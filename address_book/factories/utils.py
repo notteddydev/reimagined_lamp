cc6397f3b68fe85b15a597638090d7bc16014e30
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-09-03 07:51:53 -0400

    Update generate_fake_number method as it was occasionally causing problems. Quick fix should do the job for the timebeing.

--- address_book/factories/utils.py
+++ address_book/factories/utils.py
@@ -1,6 +1,6 @@
 import random
 
-from phone_gen import PhoneNumber as PhoneNumberGenerator
+from phone_gen import PhoneNumber as PhoneNumberGenerator, PhoneNumberNotFound
 
 from django.conf import settings
 from django.utils import translation
@@ -12,5 +12,11 @@ def generate_fake_number() -> str:
     language = translation.get_language() or settings.LANGUAGE_CODE
     choices = localized_choices(language)
     code = random.choice([code for code, _ in choices if len(code)])
-    number = PhoneNumberGenerator(code).get_number()
+    # Occasionally raises an error for code "001". 001 does not appear to be an option
+    # in the localized_choices method, and this quick fix is good enough for now as the
+    # error is only occasional.
+    try:
+        number = PhoneNumberGenerator(code).get_number()
+    except PhoneNumberNotFound:
+        generate_fake_number()
     return number
