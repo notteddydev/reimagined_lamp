16c508cf22f99a3fda47f8e243a9b7177f036bfc
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-09-13 14:54:02 -0300

    Try this.

--- address_book/urls.py
+++ address_book/urls.py
@@ -1,22 +1,22 @@
 from django.urls import path
 
-from . import views
+from .views import AddressCreateView, AddressDeleteView, AddressDetailView, AddressUpdateView, ContactCreateView, ContactDeleteView, ContactDetailView, contact_download_view, contact_list_download_view, contact_list_view, contact_qrcode_view, ContactUpdateView, TagCreateView, TagDeleteView, TagUpdateView, TenancyDeleteView
 
 urlpatterns = [
-    path("contacts/new", views.ContactCreateView.as_view(), name="contact-create"),
-    path("contacts/<int:pk>/delete", views.ContactDeleteView.as_view(), name="contact-delete"),
-    path("contacts/<int:pk>", views.ContactDetailView.as_view(), name="contact-detail"),
-    path("contacts/", views.contact_list_view, name="contact-list"),
-    path("contacts/download", views.contact_list_download_view, name="contact-list-download"),
-    path("contacts/<int:pk>/edit", views.ContactUpdateView.as_view(), name="contact-update"),
-    path("contacts/<int:pk>/qrcode", views.contact_qrcode_view, name="contact-qrcode"),
-    path("contacts/<int:pk>/download", views.contact_download_view, name="contact-download"),
-    path("tags/new", views.TagCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/edit", views.TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete", views.TagDeleteView.as_view(), name="tag-delete"),
-    path("tenancies/<int:pk>/delete", views.TenancyDeleteView.as_view(), name="tenancy-delete"),
-    path("addresses/<int:pk>/edit", views.AddressUpdateView.as_view(), name="address-update"),
-    path("addresses/new", views.AddressCreateView.as_view(), name="address-create"),
-    path("addresses/<int:pk>/delete", views.AddressDeleteView.as_view(), name="address-delete"),
-    path("addresses/<int:pk>", views.AddressDetailView.as_view(), name="address-detail"),
+    path("addresses/new", AddressCreateView.as_view(), name="address-create"),
+    path("addresses/<int:pk>/delete", AddressDeleteView.as_view(), name="address-delete"),
+    path("addresses/<int:pk>", AddressDetailView.as_view(), name="address-detail"),
+    path("addresses/<int:pk>/edit", AddressUpdateView.as_view(), name="address-update"),
+    path("contacts/new", ContactCreateView.as_view(), name="contact-create"),
+    path("contacts/<int:pk>/delete", ContactDeleteView.as_view(), name="contact-delete"),
+    path("contacts/<int:pk>", ContactDetailView.as_view(), name="contact-detail"),
+    path("contacts/<int:pk>/download", contact_download_view, name="contact-download"),
+    path("contacts/download", contact_list_download_view, name="contact-list-download"),
+    path("contacts/", contact_list_view, name="contact-list"),
+    path("contacts/<int:pk>/qrcode", contact_qrcode_view, name="contact-qrcode"),
+    path("contacts/<int:pk>/edit", ContactUpdateView.as_view(), name="contact-update"),
+    path("tags/new", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
+    path("tags/<int:pk>/edit", TagUpdateView.as_view(), name="tag-update"),
+    path("tenancies/<int:pk>/delete", TenancyDeleteView.as_view(), name="tenancy-delete"),
 ]
