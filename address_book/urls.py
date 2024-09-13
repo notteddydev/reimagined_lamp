d9af1e048dc39b212a62695bceebd5ad9c6de518
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-09-13 15:19:40 -0300

    Try this.

--- address_book/urls.py
+++ address_book/urls.py
@@ -1,22 +1,26 @@
 from django.urls import path
+import importlib
 
-from .views import AddressCreateView, AddressDeleteView, AddressDetailView, AddressUpdateView, ContactCreateView, ContactDeleteView, ContactDetailView, contact_download_view, contact_list_download_view, contact_list_view, contact_qrcode_view, ContactUpdateView, TagCreateView, TagDeleteView, TagUpdateView, TenancyDeleteView
+def get_my_model_view(my_model_view: str):
+    # Dynamically import the view module
+    views_module = importlib.import_module('myapp.views')
+    return getattr(views_module, my_model_view)
 
 urlpatterns = [
-    path("addresses/new", AddressCreateView.as_view(), name="address-create"),
-    path("addresses/<int:pk>/delete", AddressDeleteView.as_view(), name="address-delete"),
-    path("addresses/<int:pk>", AddressDetailView.as_view(), name="address-detail"),
-    path("addresses/<int:pk>/edit", AddressUpdateView.as_view(), name="address-update"),
-    path("contacts/new", ContactCreateView.as_view(), name="contact-create"),
-    path("contacts/<int:pk>/delete", ContactDeleteView.as_view(), name="contact-delete"),
-    path("contacts/<int:pk>", ContactDetailView.as_view(), name="contact-detail"),
-    path("contacts/<int:pk>/download", contact_download_view, name="contact-download"),
-    path("contacts/download", contact_list_download_view, name="contact-list-download"),
-    path("contacts/", contact_list_view, name="contact-list"),
-    path("contacts/<int:pk>/qrcode", contact_qrcode_view, name="contact-qrcode"),
-    path("contacts/<int:pk>/edit", ContactUpdateView.as_view(), name="contact-update"),
-    path("tags/new", TagCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
-    path("tags/<int:pk>/edit", TagUpdateView.as_view(), name="tag-update"),
-    path("tenancies/<int:pk>/delete", TenancyDeleteView.as_view(), name="tenancy-delete"),
+    path("contacts/new", get_my_model_view("ContactCreateView").as_view(), name="contact-create"),
+    path("contacts/<int:pk>/delete", get_my_model_view("ContactDeleteView").as_view(), name="contact-delete"),
+    path("contacts/<int:pk>", get_my_model_view("ContactDetailView").as_view(), name="contact-detail"),
+    path("contacts/", get_my_model_view("contact_list_view"), name="contact-list"),
+    path("contacts/download", get_my_model_view("contact_list_download_view"), name="contact-list-download"),
+    path("contacts/<int:pk>/edit", get_my_model_view("ContactUpdateView").as_view(), name="contact-update"),
+    path("contacts/<int:pk>/qrcode", get_my_model_view("contact_qrcode_view"), name="contact-qrcode"),
+    path("contacts/<int:pk>/download", get_my_model_view("contact_download_view"), name="contact-download"),
+    path("tags/new", get_my_model_view("TagCreateView").as_view(), name="tag-create"),
+    path("tags/<int:pk>/edit", get_my_model_view("TagUpdateView").as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete", get_my_model_view("TagDeleteView").as_view(), name="tag-delete"),
+    path("tenancies/<int:pk>/delete", get_my_model_view("TenancyDeleteView").as_view(), name="tenancy-delete"),
+    path("addresses/<int:pk>/edit", get_my_model_view("AddressUpdateView").as_view(), name="address-update"),
+    path("addresses/new", get_my_model_view("AddressCreateView").as_view(), name="address-create"),
+    path("addresses/<int:pk>/delete", get_my_model_view("AddressDeleteView").as_view(), name="address-delete"),
+    path("addresses/<int:pk>", get_my_model_view("AddressDetailView").as_view(), name="address-detail"),
 ]
