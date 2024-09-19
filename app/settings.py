41c268a3914c12d23f94f44522e38247ea5e2f9c
notteddydev <168309844+notteddydev@users.noreply.github.com>
2024-09-18 21:17:29 -0300

    Add 127.0.0.1 to allowed hosts.

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').upper() == 'TRUE'
 
-ALLOWED_HOSTS = ['reimaginedlamp.local']
+ALLOWED_HOSTS = ['127.0.0.1', 'reimaginedlamp.local']
 
 
 # Application definition
